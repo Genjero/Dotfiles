40a7f20b2d4555d30f46a10154237ef138a43542
Genjero <gafrizal414@gmail.com>
2024-04-23 16:34:16 +0700

    add lsp,linter,formatter to html,cds,js/ts and add hlslens and project plugins

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -1,15 +1,22 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "black", "autopep8" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "black", "autopep8" },
+                javascript = { "prettierd", "prettier" },
+                typescript = { "prettierd", "prettier" },
+                html = { "prettierd", "prettier" },
+                css = { "prettierd", "prettier" },
+
+
+
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+        })
+    end,
 }
--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,25 +1,34 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local lspconfig = require("lspconfig")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local lspconfig = require("lspconfig")
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-			capabilities = capabilities,
-		})
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-		})
-		lspconfig.pylsp.setup({
-			capabilities = capabilities,
-		})
-	end,
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+            capabilities = capabilities,
+        })
+        lspconfig.pyright.setup({
+            capabilities = capabilities,
+        })
+        lspconfig.pylsp.setup({
+            capabilities = capabilities,
+        })
+        lspconfig.tsserver.setup({
+            capabilities = capabilities,
+        })
+        lspconfig.html.setup({
+            capabilities = capabilities,
+        })
+        lspconfig.cssls.setup({
+            capabilities = capabilities,
+        })
+    end,
 }
